6e4ad3832080a814e549a902219192524b695268
safo0o999 <safwan.m.noor@gmail.com>
2026-07-11 23:38:36 +0300

    Add REQUIRED_COLUMNS dictionary to schema.py

---
+++ config/schema.py
@@ -0,0 +1,41 @@
+REQUIRED_COLUMNS = {
+
+    "ASN": [
+        "GTIN",
+        "BN",
+        "Expiry Date",
+        "Trade Name",
+        "Received Quantity"
+    ],
+
+    "INVENTORY": [
+        "GTIN",
+        "BN",
+        "Expiry Date",
+        "Available Quantity"
+    ],
+
+    "DISPATCH": [
+        "GTIN",
+        "BN",
+        "Expiry Date",
+        "Dispatched Quantity"
+    ],
+
+    "SFDA": [
+        "GTIN",
+        "BN",
+        "Expiry Date",
+        "Drug Name",
+        "PZ",
+        "Active",
+        "Quantity Receive Pending",
+        "Quantity sent pending"
+    ],
+
+    "PACKSIZE": [
+        "Trade Name",
+        "PackageSize"
+    ]
+
+}
