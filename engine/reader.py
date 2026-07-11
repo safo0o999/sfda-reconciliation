427d5ed21f8c1421af119a7707184ab2b847884c
safo0o999 <safwan.m.noor@gmail.com>
2026-07-11 23:39:44 +0300

    Add ExcelReader class for reading Excel files
    Implement ExcelReader class to read Excel files with support for .xls and .xlsx formats.

---
+++ engine/reader.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import pandas as pd
+
+
+class ExcelReader:
+
+    @staticmethod
+    def get_engine(file_path: Path):
+
+        if file_path.suffix.lower() == ".xls":
+            return "xlrd"
+
+        return "openpyxl"
+
+    @staticmethod
+    def read(file_path):
+
+        file_path = Path(file_path)
+
+        if not file_path.exists():
+            raise FileNotFoundError(file_path)
+
+        return pd.read_excel(
+            file_path,
+            engine=ExcelReader.get_engine(file_path),
+            dtype=object
+        )
