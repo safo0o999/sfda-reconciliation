1f71a21f151eb0b8b4f61ee9dd74b56bb3790edd
safo0o999 <safwan.m.noor@gmail.com>
2026-07-12 09:07:05 +0300

    Refactor summarize method to store grouped result

--- engine/grouper.py
+++ engine/grouper.py
@@ -3,10 +3,13 @@ class Grouper:
     @staticmethod
     def summarize(df, keys, quantity_column):
 
-        return (
+        grouped = (
             df.groupby(
                 keys,
+                dropna=False,
                 as_index=False
             )[quantity_column]
             .sum()
         )
+
+        return grouped
