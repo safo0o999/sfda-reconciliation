2377ca65c0780ea9ee0866fe5393c898735a27e7
safo0o999 <safwan.m.noor@gmail.com>
2026-07-18 10:31:00 +0300

    Reformat REQUIRED_COLUMNS for better readability

--- config/schema.py
+++ config/schema.py
@@ -1,13 +1,39 @@
 REQUIRED_COLUMNS = {
-    "ASN": ["BN", "Expiry Date", "Trade Name", "Received Quantity", "Generic Item Number"],
-    "INVENTORY": ["BN", "Expiry Date", "Trade Name", "Available Quantity", "Generic Item Number"],
+    "ASN": [
+        "BN",
+        "Expiry Date",
+        "Trade Name",
+        "Received Quantity",
+        "Generic Item Number",
+    ],
+    "INVENTORY": [
+        "BN",
+        "Expiry Date",
+        "Trade Name",
+        "Available Quantity",
+        "Generic Item Number",
+    ],
     "DISPATCH": [
-        "BN", "Expiry Date", "Trade Name", "Dispatched Quantity",
-        "To Address", "Sales Order Number", "Generic Item Number"
+        "BN",
+        "Expiry Date",
+        "Trade Name",
+        "Dispatched Quantity",
+        "To Address",
+        "Sales Order Number",
+        "Generic Item Number",
     ],
     "SFDA": [
-        "GTIN", "Drug Name", "BN", "Expiry Date", "Quantity", "Active",
-        "Quantity Receive Pending", "Quantity sent pending"
+        "GTIN",
+        "Drug Name",
+        "BN",
+        "Expiry Date",
+        "Quantity",
+        "Active",
+        "Quantity Receive Pending",
+        "Quantity sent pending",
     ],
-    "PACKSIZE": ["Trade Name", "PackageSize"],
+    "PACKSIZE": [
+        "Trade Name",
+        "PackageSize",
+    ],
 }
