aaa1f20e0b2aa34acb9ac44066dc8600f52c4a1d
safo0o999 <safwan.m.noor@gmail.com>
2026-07-18 09:27:31 +0300

    Add 'Generic Item Number' to required columns

--- config/schema.py
+++ config/schema.py
@@ -1,13 +1,13 @@
 REQUIRED_COLUMNS = {
-    "ASN": ["BN", "Expiry Date", "Trade Name", "Received Quantity"],
-    "INVENTORY": ["BN", "Expiry Date", "Trade Name", "Available Quantity"],
+    "ASN": ["BN", "Expiry Date", "Trade Name", "Received Quantity", "Generic Item Number"],
+    "INVENTORY": ["BN", "Expiry Date", "Trade Name", "Available Quantity", "Generic Item Number"],
     "DISPATCH": [
         "BN", "Expiry Date", "Trade Name", "Dispatched Quantity",
-        "To Address", "Sales Order Number"
+        "To Address", "Sales Order Number", "Generic Item Number"
     ],
     "SFDA": [
         "GTIN", "Drug Name", "BN", "Expiry Date", "Quantity", "Active",
         "Quantity Receive Pending", "Quantity sent pending"
     ],
-    "PACKSIZE": ["Trade Name", "PackageSize"]
+    "PACKSIZE": ["Trade Name", "PackageSize"],
 }
